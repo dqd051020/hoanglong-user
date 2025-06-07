17030c43ae711b8d9ac51b3791d83972abd46f53
dqd051020 <103426479+dqd051020@users.noreply.github.com>
2025-06-07 18:13:25 +0700

    Update page.tsx

--- src/app/doi-mat-khau/page.tsx
+++ src/app/doi-mat-khau/page.tsx
@@ -2,7 +2,7 @@ import ChangePassComponent from '@/components/change-pass/change-pass';
 import { NextPage } from 'next';
 
 const ChangePass: NextPage = () => {
-  return <ChangePassComponent />;  //hay quá
+  return <ChangePassComponent />;  
 };
 
 export default ChangePass;
